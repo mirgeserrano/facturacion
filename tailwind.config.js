298d902e61c810df514834f08de211a5122dc2a5
mirge <mirgeserrano@gmail.com>
2024-02-13 14:27:31 -0400

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //apunta a todos los archivo que va a transformar
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
